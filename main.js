39b41c30eefa705e7519ab23c7616cce8934b50a
David Konsumer <konsumer@jetboystudio.com>
2016-03-28 20:15:13 -0700

    added prompt

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
+const ipcMain = electron.ipcMain
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    preload: __dirname + '/prompt.js'
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
@@ -26,26 +32,39 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+    app.quit();
   });
+
+  var promptResponse
+  ipcMain.on('prompt', function(eventRet, arg) {
+    promptResponse = null
+    var promptWindow = new BrowserWindow({
+      width: 200,
+      height: 100,
+      show: false,
+      resizable: false,
+      movable: false,
+      alwaysOnTop: false,
+      frame: false
+    })
+    arg.val = arg.val || ''
+    const promptHtml = '<label for="val">' + arg.title + '</label>\
+    <input id="val" value="' + arg.val + '" autofocus />\
+    <button onclick="window.close()">Cancel</button>\
+    <button onclick="require(\'electron\').ipcRenderer.send(\'prompt-response\', document.getElementById(\'val\').value);window.close()">Ok</button>\
+    <style>button {float:right;} label,input {margin-bottom: 5px; width: 100%; display:block}</style>'
+    promptWindow.loadURL('data:text/html,' + promptHtml)
+    promptWindow.show()
+    promptWindow.on('closed', function() {
+      eventRet.returnValue = promptResponse
+    })
+  })
+  ipcMain.on('prompt-response', function(event, arg) {
+    if (arg === ''){ arg = null }
+    promptResponse = arg
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
